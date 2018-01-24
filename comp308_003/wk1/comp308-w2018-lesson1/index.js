d14b6cf2a1d876abb99f98bfb8ae06412a2321b5
Kevin Ma <kbma.kevin@gmail.com>
2018-01-24 14:46:20 -0500

    added nodemon - NOTE: only works if i installed globally, not as devDependency

--- comp308_003/wk1/comp308-w2018-lesson1/index.js
+++ comp308_003/wk1/comp308-w2018-lesson1/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/home', (req, res) => res.send('You are at home!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
