f929f35eb492cbba234923cbeea688204944dafe
Kevin Ma <kbma.kevin@gmail.com>
2018-03-04 15:18:17 -0500

    added Feedback Model

--- comp308_003/KevinMa_COMP308Lab2/app/models/customer.server.model.js
+++ comp308_003/KevinMa_COMP308Lab2/app/models/customer.server.model.js
@@ -4,33 +4,45 @@ const Schema = mongoose.Schema;
 let CustomerSchema = new Schema({
     firstName: {
         type: String,
+        required: true,
         trim: true,
         // secondary index created because application will use alot of queries involving customer's first and last name; optimize query execution
         index: true
     },
     lastName: {
         type: String,
+        required: true,
         trim: true,
         index: true
     },
     email: {
         type: String,
+        required: true,
         trim: true,
+        // predefined validator
+        match: /.+\@.+\..+/,
         // tells MongoDB to create unique index for the email field of the customers collection
         unique: true
     },
     password: {
         type: String,
+        required: true,
         trim: true,
-
+        // custom validator - array of validation function, err msg to return to callback
+        validate: [
+            (password) => password.length >= 5,
+            'Password should be at least 5 characters long!'
+        ],
     },
     // two custom fields for assignment 2
     age: {
         type: Number,
+        required: true,
         // min: 0
     },
     program: {
         type: String,
+        required: true,
         trim: true,
         uppercase: true
     },
@@ -55,6 +67,23 @@ CustomerSchema.statics.findOneByEmail = function (email, callback) {
 CustomerSchema.methods.authenticate = function (password) {
     return this.password === password;
 }
+
+// Mongoose middleware can intercept process of the: init, validate, save, and remove instance methods
+
+// Pre-Middleware
+CustomerSchema.pre('save', (next) => {
+    // things to do before saving document e.g. complex validation here...
+});
+
+// Post-Middleware
+CustomerSchema.post('save', function (next) {
+    // perfect for logging application logic
+    if (this.isNew) {
+        console.log('A new user was created ' + this.email);
+    } else {
+        console.log(this.email + ' has updated its details');
+    }
+});
 
 // Configure Schema to include virtual attributes and getter methods when converting the MongoDB document to a JSON representation, and will allow the output of documents using res.json() to include the getter's behavior
 CustomerSchema.set('toJSON', { virtuals: true, getters: true });
---
+++ comp308_003/KevinMa_COMP308Lab2/app/models/feedback.server.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let FeedbackSchema = new Schema({
+    content: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    author: {
+        // Mongoose DBRef simulates joins of RDBMS joins
+        // 1. using the ObjectID schema type and the ref property  to create DBRef
+        // 2. later using populate to gather the data when retrieving the document
+        type: Schema.Types.ObjectId,
+        ref: 'Customer',
+    }
+});
+
+// // Pre-Middleware
+// FeedbackSchema.pre('save', (next) => {
+//     // things to do before saving document e.g. complex validation here...
+
+//     // find customer, and add it as this feedback's author before saving
+// });
+
+// define the Feedback model
+mongoose.model('Feedback', FeedbackSchema);
